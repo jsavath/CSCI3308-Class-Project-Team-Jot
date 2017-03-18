4f73c5e39b4a35d645b32a62e08bc9371dd763ff
Hussain Quadri <HussainQ@users.noreply.github.com>
2017-03-18 13:50:47 -0600

    Updating InsertHandler
    Edited Insert handler from lab to accommodate for new simple_homepage.

--- SQLInsertHandler.php
+++ SQLInsertHandler.php
@@ -3,8 +3,8 @@
 //Lab 8
 
 // Obtain a connection object by connecting to the db
-$connection = @mysqli_connect ('127.0.0.1', 'root',
-'password', 'lab8'); // please fill these parameters with the actual data
+$connection = @mysqli_connect ('localhost', 'root',
+'password', 'simple_jotdb'); // please fill these parameters with the actual data
 if(mysqli_connect_errno())
 {
  echo "<h4>Failed to connect to MySQL: </h4>".mysqli_connect_error();
@@ -16,16 +16,18 @@ else
 
 //Variables
 $Id = $_REQUEST['Id'];
-$Name = $_REQUEST['Name'];
-$Quantity = $_REQUEST['Quantity'];
-$Price = $_REQUEST['Price'];
+$First_Name = $_REQUEST['First_Name'];
+$Last_Name = $_REQUEST['Last_Name'];
+$Course_Id = $_REQUEST['Course_Id'];
+$Course_Name = $_REQUEST['Course_Name'];
+$Notes = $_REQUEST['Notes'];
 
 //Query
-$query = "INSERT INTO store VALUES ('$Id', '$Name', '$Quantity','$Price');";
+$query = "INSERT INTO simple_table VALUES ('$Id', '$First_Name', '$Last_Name','$Course_Id', '$Course_Name', '$Notes');";
 
 
 //Submit query
 mysqli_query($connection,$query);
 echo "Inserted successfully into the database";
-include 'store.php';
+include 'simple_homepage.php';
 ?>
