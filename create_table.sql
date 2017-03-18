4c12946a60dc9cbb4294582af8357696713616d3
BryanG1019 <brge5762@colorado.edu>
2017-03-18 15:04:00 -0600

    Add files via upload

---
+++ create_table.sql
@@ -0,0 +1,9 @@
+create table if not exists `simple_table` (
+ `student_Id` int(9) not null,
+ `first_Name` varchar(10) not null,
+ `last_Name` varchar(10) not null,
+ `course_Id` int(4) not null,
+ `course_Name` varchar(10) not null,
+ `notes` varchar(100) not null,
+ primary key (`student_Id`)
+) 
