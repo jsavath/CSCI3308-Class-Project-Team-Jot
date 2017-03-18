3fe48443b5f5f9a0dc2272559db4d1ccc95cde6b
jsavath <jason.savath@colorado.edu>
2017-03-18 13:52:26 -0600

    update table names

--- simple_homepage.php
+++ simple_homepage.php
@@ -51,17 +51,17 @@ value=\"delete\"/><br>";
 
 <form enctype="multipart/form-data"
 action="http://localhost/SQLInsertHandler.php">
-<p>Student Id:&nbsp <input type="text" name="Id" size="10" maxlength="11"
+<p>Student Id:&nbsp <input type="text" name="student_Id" size="10" maxlength="11"
 /></p>
-<p>First Name:&nbsp <input type="text" name="First_Name" size="10"
+<p>First Name:&nbsp <input type="text" name="first_Name" size="10"
 maxlength="20" /></p>
-<p>Last Name:&nbsp <input type=”text” name=”Last_Name” size=”10” 
+<p>Last Name:&nbsp <input type=”text” name=”last_Name” size=”10” 
 maxlength=”20” /></p>
-<p>Course Id:&nbsp <input type="text" name="Course_Id" size="10"
+<p>Course Id:&nbsp <input type="text" name="course_Id" size="10"
 maxlength="10" /></p>
-<p>Course Name:&nbsp <input type=”text” name=”Course_Name” size=”10” 
+<p>Course Name:&nbsp <input type=”text” name=”course_Name” size=”10” 
 maxlength=”20” /></p>
-<p>Notes:&nbsp <input type=”text” name=”Notes” size=”50” 
+<p>Notes:&nbsp <input type=”text” name=”notes” size=”50” 
 maxlength=”100” /></p>
 <br>
 <input type="submit" value="Add item" /> &nbsp
