6f498816f424ff6c4f59a3048f2fb36371e688f3
Hussain Quadri <HussainQ@users.noreply.github.com>
2017-03-18 14:19:19 -0600

    Changed quotation marks to ""
    Fixed variable insert boxes

--- simple_homepage.php
+++ simple_homepage.php
@@ -55,14 +55,14 @@ action="http://localhost/SQLInsertHandler.php">
 /></p>
 <p>First Name:&nbsp <input type="text" name="first_Name" size="10"
 maxlength="20" /></p>
-<p>Last Name:&nbsp <input type=”text” name=”last_Name” size=”10” 
-maxlength=”20” /></p>
+<p>Last Name:&nbsp <input type="text" name="last_Name" size="10" 
+maxlength="20" /></p>
 <p>Course Id:&nbsp <input type="text" name="course_Id" size="10"
 maxlength="10" /></p>
-<p>Course Name:&nbsp <input type=”text” name=”course_Name” size=”10” 
-maxlength=”20” /></p>
-<p>Notes:&nbsp <input type=”text” name=”notes” size=”50” 
-maxlength=”100” /></p>
+<p>Course Name:&nbsp <input type="text" name="course_Name" size="10" 
+maxlength="20" /></p>
+<p>Notes:&nbsp <input type="text" name="notes" size="50" 
+maxlength="100" /></p>
 <br>
 <input type="submit" value="Add item" /> &nbsp
 <input type="reset" />
