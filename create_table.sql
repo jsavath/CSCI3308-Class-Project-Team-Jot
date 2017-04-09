ce731a65c1adff10824b832a5f63e3145b8156dd
jsavath <jason.savath@colorado.edu>
2017-04-09 13:56:34 -0600

    Update create table with user pass

--- create_table.sql
+++ create_table.sql
@@ -1,5 +1,6 @@
 create table if not exists `simple_table` (
  `student_Id` int(9) not null,
+ `PasswordHash` BINARY(64) not null,
  `first_Name` varchar(10) not null,
  `last_Name` varchar(10) not null,
  `course_Id` int(4) not null,
