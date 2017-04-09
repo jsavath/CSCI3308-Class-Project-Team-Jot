0f5f457c181598d7266656b2f8b16f15524ba9c7
jsavath <jason.savath@colorado.edu>
2017-04-09 14:25:13 -0600

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,6 +1,6 @@
 create table if not exists `simple_table` (
  `student_Id` int(9) not null,
- `PasswordHash` BINARY(64) not null,
+ `PasswordHash` NVARCHAR(50) not null,
  `first_Name` varchar(10) not null,
  `last_Name` varchar(10) not null,
  `course_Id` int(4),
