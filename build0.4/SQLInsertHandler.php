a8db2f2d0a7c4bab75bf00d3f45d2f549c111077
Jason Savath <jason.savath@colorado.edu>
2017-04-13 14:40:16 -0600

    created version 0.4 working with instructions

---
+++ build0.4/SQLInsertHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+// Obtain a connection object by connecting to the db
+$connection = @mysqli_connect ('localhost', 'root',
+'password', 'jot'); // please fill these parameters with the actual data
+// if(mysqli_connect_errno())
+// {
+//  echo "<h4>Failed to connect to MySQL: </h4>".mysqli_connect_error();
+// }
+// else
+// {
+//  echo "<h4>Successfully connected to MySQL: </h4>";
+// }
+
+//Variables
+$student_Id = $_REQUEST['student_Id'];
+$PasswordHash = $_REQUEST['PasswordHash'];
+$first_Name = $_REQUEST['first_Name'];
+$last_Name = $_REQUEST['last_Name'];
+
+//Query
+$query = "INSERT INTO simple_table VALUES ('$student_Id','$PasswordHash','$first_Name','$last_Name');";
+
+
+//Submit query
+mysqli_query($connection,$query);
+include 'created_redirect.html';
+?>
